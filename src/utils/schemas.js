7e6f2d3a00e5ee57af706121f13141ff87ce9b67
tranlequangtrungg-lang <tranlequangtrungg@gmail.com>
2026-08-10 09:21:27 +0700

    Update schemas.js

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -84,6 +84,9 @@ export const EconomyDataSchema = z
   .object({
     wallet: z.number().nonnegative().default(0),
     bank: z.number().nonnegative().default(0),
+    dailyBetBudget: z.number().nonnegative().default(0),
+    dailyBetTotal: z.number().nonnegative().default(0),
+    dailyBetResetAt: z.number().int().nonnegative().default(0),
     bankLevel: z.number().int().nonnegative().default(0),
     dailyStreak: z.number().int().nonnegative().default(0),
     lastDaily: z.number().int().nonnegative().default(0),
