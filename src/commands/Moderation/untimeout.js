e36fd6c6b24dfb0872be1f1672d9a21caac2dee2
tranlequangtrungg-lang <tranlequangtrungg@gmail.com>
2026-08-18 22:59:01 +0700

    Update untimeout.js

--- src/commands/Moderation/untimeout.js
+++ src/commands/Moderation/untimeout.js
@@ -4,61 +4,54 @@ import { logger } from '../../utils/logger.js';
 import { ModerationService } from '../../services/moderation/moderationService.js';
 import { TitanBotError, ErrorTypes } from '../../utils/errorHandler.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
-
 export default {
     data: new SlashCommandBuilder()
-        .setName("untimeout")
-        .setDescription("Remove timeout from a user")
+        .setName("thatungay")
+        .setDescription("Gỡ tống ngay khỏi một thành viên")
         .addUserOption((option) =>
             option
                 .setName("target")
-                .setDescription("User to untimeout")
+                .setDescription("Người cần gỡ")
                 .setRequired(true),
         )
         .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
     category: "moderation",
-
     async execute(interaction, config, client) {
         const deferSuccess = await InteractionHelper.safeDefer(interaction);
         if (!deferSuccess) {
-            logger.warn(`Untimeout interaction defer failed`, {
+            logger.warn(`Thatungay interaction defer failed`, {
                 userId: interaction.user.id,
                 guildId: interaction.guildId,
-                commandName: 'untimeout',
+                commandName: 'thatungay',
             });
             return;
         }
-
         const targetUser = interaction.options.getUser("target");
         const member = interaction.options.getMember("target");
-
         if (!targetUser) {
             throw new TitanBotError(
                 'Missing target user',
                 ErrorTypes.USER_INPUT,
-                'You must specify a user to untimeout.',
+                'Bạn phải chọn người để gỡ tống ngay.',
                 { subtype: 'invalid_user' },
             );
         }
-
         if (!member) {
             throw new TitanBotError(
                 "Target not found",
                 ErrorTypes.USER_INPUT,
-                "The target user is not currently in this server.",
+                "Người này hiện không có trong server.",
             );
         }
-
         await ModerationService.removeTimeoutUser({
             guild: interaction.guild,
             member,
             moderator: interaction.member,
         });
-
         await InteractionHelper.safeEditReply(interaction, {
             embeds: [
                 successEmbed(
-                    `🔓 **Removed timeout** from ${targetUser.tag}`,
+                    `🔓 **Đã gỡ tống ngay** khỏi ${targetUser.tag}`,
                 ),
             ],
         });
